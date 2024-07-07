fbf3bc63fb2ff3abe4113f7e361e864b83b33d0c
bullbearyBots <bullbeary.bots@gmail.com>
2024-07-07 22:31:21 +0200

    init project and added few functionalities

---
+++ 02-backend/ecommerce/src/main/java/com/bullbearybots/ecommerce/config/DataRestConfig.java
@@ -0,0 +1,31 @@
+package com.bullbearybots.ecommerce.config;
+
+import com.bullbearybots.ecommerce.entity.Product;
+import com.bullbearybots.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class DataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // disable HTTP methods for Product
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // disable HTTP methods for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
